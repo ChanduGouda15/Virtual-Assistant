04f444f83767d11674f57c132f394167b7c76573
chandu gouda <chandugouda15@gmail.com>
2025-10-20 21:53:27 +0530

    Restore main entry setup; ensure React app bootstraps correctly.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
-import UserContextProvider from './context/userContext.jsx'; 
-
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {BrowserRouter} from "react-router-dom"
+import UserContext from './context/UserContext.jsx'
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <UserContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </UserContextProvider>
-  </StrictMode>
-);
+<BrowserRouter>
+<UserContext>
+    <App />
+  </UserContext>
+  </BrowserRouter>
+ 
+)
