8c49b2893dfc1d54415dfa05094e935d3e214895
chandu gouda <chandugouda15@gmail.com>
2025-10-20 21:52:40 +0530

    Revert update route; remove Multer hook.

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,15 +1,12 @@
-import express from 'express';
-import { askToAssistant, getCurrentUser, updateAssistant } from '../controllers/user.controller.js';
+import express from "express"
+import { askToAssistant, getCurrentUser, updateAssistant } from "../controllers/user.controller.js"
 import isAuth from "../middlewares/isAuth.js"
-import multer from 'multer';
+import upload from "../middlewares/multer.js"
 
-const storage = multer.memoryStorage(); // or diskStorage() if you want to save to disk
-const upload = multer({ storage });
-
-
-const userRouter = express.Router()
+const userRouter=express.Router()
 
 userRouter.get("/current",isAuth,getCurrentUser)
 userRouter.post("/update",isAuth,upload.single("assistantImage"),updateAssistant)
 userRouter.post("/asktoassistant",isAuth,askToAssistant)
-export default userRouter;
+
+export default userRouter
